62d9bec75cf56f5d76c7641a7737020062d460f9
Tchoupinax <corentinfiloche@hotmail.fr>
2023-08-19 08:13:06 +0200

    feat: add new metric latest file size

---
+++ src/metrics/metric-latest-file-size.spec.ts
@@ -0,0 +1,36 @@
+import { _Object } from "@aws-sdk/client-s3";
+import { beforeEach, describe, expect, it } from "vitest";
+
+import MetricLatestFileSize from "./metric-latest-file-size";
+
+describe("metric latest file size", () => {
+  let metricLatestFileSize: MetricLatestFileSize;
+
+  beforeEach(() => {
+    metricLatestFileSize = new MetricLatestFileSize("test");
+  });
+
+  it("should return the size of the latest file (sorted by timestamp)", () => {
+    const data: Array<_Object> = [
+      { Size: 23, LastModified: new Date("December 17, 1992") },
+      { Size: 1234, LastModified: new Date("December 17, 1998") },
+      { Size: 2345, LastModified: new Date("December 17, 1995") },
+    ];
+
+    const answer = metricLatestFileSize.process(data);
+
+    expect(answer).toBe(1234);
+  });
+
+  it("should return the size of the latest file (sorted by timestamp)", () => {
+    const data: Array<_Object> = [
+      { Size: 23, LastModified: new Date("December 17, 1992") },
+      { Size: 1234, LastModified: new Date("December 17, 1988") },
+      { Size: 2345, LastModified: new Date("December 17, 1995") },
+    ];
+
+    const answer = metricLatestFileSize.process(data);
+
+    expect(answer).toBe(2345);
+  });
+});
---
+++ src/metrics/metric-latest-file-size.ts
@@ -0,0 +1,34 @@
+import { _Object } from "@aws-sdk/client-s3";
+import { Gauge, Registry } from "prom-client";
+
+import Metric from "./metric";
+
+export default class extends Metric {
+  constructor (prefix: string) {
+    super("latest_file_size", prefix);
+  }
+
+  declarePrometheusMesure (register: Registry): Gauge<any> {
+    return new Gauge({
+      name: this.name(),
+      help: "Most recent file size",
+      labelNames: ["name"],
+      registers: [register],
+    });
+  }
+
+  process (files: Array<_Object>): number {
+    let mostRecentFile: _Object = files[0];
+
+    for (const file of files) {
+      if (
+        file?.LastModified && mostRecentFile?.LastModified &&
+        file?.LastModified > mostRecentFile?.LastModified
+      ) {
+        mostRecentFile = file;
+      }
+    }
+
+    return mostRecentFile?.Size ?? -1;
+  }
+}
