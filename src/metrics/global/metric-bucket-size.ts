5c0c4660432c30cda7eb3374b4633ee309181a14
Tchoupinax <corentinfiloche@hotmail.fr>
2023-12-26 09:20:05 +0100

    feat: calculate the size of the bucket (#13)
    * feat: calculate the size of the bucket
    
    * chore: upgrade deps
    
    * chore: prepare v0.0.4

---
+++ src/metrics/global/metric-bucket-size.ts
@@ -0,0 +1,25 @@
+import { _Object } from "@aws-sdk/client-s3";
+import { Gauge, Registry } from "prom-client";
+
+import { Metric } from "../metric";
+
+export default class extends Metric {
+  constructor () {
+    super("bucket_size", "global");
+  }
+
+  declarePrometheusMesure (register: Registry): Gauge<any> {
+    return new Gauge({
+      name: this.name(),
+      help: "Size of the bucket (in Bytes)",
+      labelNames: ["name"],
+      registers: [register],
+    });
+  }
+
+  process (files: _Object[]): number {
+    return files.reduce((acc: number, cur: _Object) => {
+      return cur.Size! + acc;
+    }, 0);
+  }
+}
